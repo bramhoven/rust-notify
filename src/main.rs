9e288ffb597930f8a6ec11a88402fc62124d42f6
Bram Hoven <bram.hoven@hotmail.com>
2024-09-19 11:53:41 +0200

    Add CLI parsing

--- src/main.rs
+++ src/main.rs
@@ -3,23 +3,39 @@ use axum::{
     http::StatusCode,
     Router,
 };
+use clap::Parser;
+
+#[derive(Parser, Debug)]
+#[command(version, about, long_about=None)]
+struct CommandLineArguments {
+    /// host to listen on
+    #[arg(short='u', long="host", default_value="0.0.0.0")]
+    host: String,
+
+    /// port to listen on
+    #[arg(short='p', long="port", default_value_t=3000)]
+    port: i16,
+}
 
 #[tokio::main]
 async fn main() {
-    // initialize tracing
+    let args = CommandLineArguments::parse();
+
     tracing_subscriber::fmt::init();
 
-    // build our application with a route
     let app = Router::new()
-        // `GET /` goes to `root`
         .route("/", get(root));
 
-    // run our app with hyper, listening globally on port 3000
-    let listener = tokio::net::TcpListener::bind("0.0.0.0:3000").await.unwrap();
+    // Create bind string: 0.0.0.0:3000
+    let mut bind_str: String = args.host;
+    bind_str.push_str(":");
+    bind_str.push_str(args.port.to_string().as_str());
+
+    println!("Server started on: {}", bind_str);
+    let listener = tokio::net::TcpListener::bind(bind_str).await.unwrap();
     axum::serve(listener, app).await.unwrap();
 }
 
-// basic handler that responds with a static string
 async fn root() -> (StatusCode, &'static str) {
     (StatusCode::OK, "Hello, World!")
 }
