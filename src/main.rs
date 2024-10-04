733bd6e1e8d16aab971787400de7ced2af6bfbef
Bram Hoven <bram.hoven@hotmail.com>
2024-10-04 14:31:27 +0200

    Fix some issues with diesel

--- src/main.rs
+++ src/main.rs
@@ -15,6 +15,7 @@ async fn main() {
     dotenv().ok();
     let args = cli::arguments::CommandLineArguments::parse();
 
+    // Configure tracing logging
     tracing_subscriber::fmt()
         .with_max_level(Level::DEBUG)
         .init();
@@ -24,14 +25,16 @@ async fn main() {
     bind_str.push_str(":");
     bind_str.push_str(args.port.to_string().as_str());
 
-
+    // Create database connection
     let database_url = env::var("DATABASE_URL").expect("DATABASE_URL must be set");
-    let conn = repository::database::establish_connection(&database_url);
+    let _conn = repository::database::establish_connection(&database_url);
 
+    // Create server
     let server = app::create_app().await;
 
     info!(target: "rust-notify", "starting server on: {}", bind_str);
 
+    // Start tokio listener
     let listener = tokio::net::TcpListener::bind(bind_str).await.unwrap();
     axum::serve(listener, server).await.unwrap();
 }
--- src/repository/entities/topic_entity.rs
+++ src/repository/entities/topic_entity.rs
@@ -2,8 +2,8 @@ use diesel::prelude::*;
 use uuid::Uuid;
 
 #[derive(Queryable, Selectable)]
-#[diesel(table_name = "topics")]
-#[diesel(check_for_backend(diesel::pg:Pg))]
+#[diesel(table_name = crate::repository::schema::topics)]
+#[diesel(check_for_backend(diesel::pg::Pg))]
 pub struct TopicEntity {
     pub id: Uuid,
     pub name: String,
